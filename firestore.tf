4f2bd8f7576ab7ac66ca16f08ba232b924884746
chenggang liu@g <liuchenggang@google.com>
2021-01-12 07:14:35 +0000

    change the gae location to us-central

--- firestore.tf
+++ firestore.tf
@@ -1,5 +1,5 @@
 resource google_app_engine_application "app" {
   # provider      = google-beta
-  location_id   = var.gcp_region
+  location_id   = "us-central"
   database_type = "CLOUD_FIRESTORE"
 }
