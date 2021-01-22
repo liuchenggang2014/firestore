279dd5b293e4043a609e6149b69ead20871ba9d2
chenggang liu@g <liuchenggang@google.com>
2021-01-22 03:07:26 +0000

    add service depends on

--- oauth.tf
+++ oauth.tf
@@ -2,6 +2,10 @@
 resource "google_iap_brand" "project_brand" {
   support_email     = "cliu201-sa@cliu201.iam.gserviceaccount.com"
   application_title = "Lego"
+
+  depends_on = [
+    google_project_service.iap
+  ]
 }
 
 
